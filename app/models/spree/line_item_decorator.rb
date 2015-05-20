3e87048a9a4e0f0a4c6ebd5cd978a87dde9002d4
Sergey Rublev <narma.nsk@gmail.com>
2015-05-21 05:05:52 +0600

    Fix all tests. Fix line_item sync with NG
    Fix case when we double delete items from NG
    it is covered by order_spec.rb:69

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,31 +1,17 @@
 Spree::LineItem.class_eval do
 
-  after_create :add_to_newgistics_shipment_contents, if: lambda {|line_item| line_item.order.can_update_newgistics? }
-
-  after_destroy :remove_from_newgistics_shipment_contents, if: lambda {|line_item| line_item.order.can_update_newgistics? }
-
-  after_update :update_newgistics_shipment_contents, if: lambda { |line_item| line_item.quantity_changed? && line_item.order.can_update_newgistics?}
-
-
-  ## after a line item is added to the order and if the order state is completed, update newgistics   ## shipment.
-  def add_to_newgistics_shipment_contents(qty = self.quantity)
-    order.add_newgistics_shipment_content(variant.sku, qty)
+  durably_decorate :update_inventory do
+    sync_to_newgistics_if_needed
+    original_update_inventory
   end
 
-  ## after a line item is removed to the order and if the order state is completed update newgistics  ## shipment.
-  def remove_from_newgistics_shipment_contents(qty = self.quantity)
-    return if qty == 0 # see order_inventory_decorator
-    order.remove_newgistics_shipment_content(variant.sku, qty)
-  end
-
-  ## If line items quantities change and the order state is completed, update newgistics shipment
-  def update_newgistics_shipment_contents
-    old_value = changes[:quantity][0]
-    new_value = changes[:quantity][1]
-    if old_value > new_value
-      remove_from_newgistics_shipment_contents(old_value - new_value)
+  def sync_to_newgistics_if_needed
+    return unless self.order.can_update_newgistics?
+    diff = self.quantity - self.inventory_units.size
+    if diff > 0
+      order.add_newgistics_shipment_content(variant.sku, diff)
     else
-      add_to_newgistics_shipment_contents(new_value - old_value)
+      order.remove_newgistics_shipment_content(variant.sku, -diff)
     end
   end
 end
--- spec/models/spree/variant_spec.rb
+++ spec/models/spree/variant_spec.rb
@@ -20,6 +20,7 @@ describe Spree::Order do
 
     before(:each) do
       Spree::Newgistics::HTTPManager.stub(:adapter).and_return(success_adapter)
+      Spree::Variant.any_instance.stub(:can_post_to_newgistics).and_return(true)
       @variant = create(:variant)
     end
 
