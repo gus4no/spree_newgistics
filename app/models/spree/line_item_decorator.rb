137e0bf2470d290f3483ae0b77a6706f2df1e0e6
Sergey Rublev <narma.nsk@gmail.com>
2015-05-21 23:06:57 +0600

    some style fixes

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -3,17 +3,16 @@ Spree::LineItem.class_eval do
   alias_method :old_update_inventory, :update_inventory
 
   def update_inventory
-    sync_to_newgistics_if_needed
+    sync_to_newgistics if self.order.can_update_newgistics?
     old_update_inventory    
   end
 
-  def sync_to_newgistics_if_needed
-    return unless self.order.can_update_newgistics?
+  def sync_to_newgistics
     diff = self.quantity - self.inventory_units.size
     if diff > 0
       order.add_newgistics_shipment_content(variant.sku, diff)
     else
-      order.remove_newgistics_shipment_content(variant.sku, -diff)
+      order.remove_newgistics_shipment_content(variant.sku, diff.abs)
     end
   end
 end
