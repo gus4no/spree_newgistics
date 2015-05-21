8cf521907415609240ecf135d32c51c7b9a8dd63
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-05-21 22:58:20 +0300

    load orders with line_items

--- app/async/workers/inventory_puller.rb
+++ app/async/workers/inventory_puller.rb
@@ -16,16 +16,16 @@ module Workers
 
     def update_inventory(newgistics_stock_items)
       # preload line_items of unsynced orders
-      unsynced_line_items = Spree::Order.not_in_newgistics.collect { |os| os.line_items }.flatten
+      unsynced_line_items = Spree::Order.not_in_newgistics.includes(:line_items).collect { |os| os.line_items }.flatten
 
       log = File.open("#{Rails.root}/log/#{self.jid}_newgistics_inventory_import.log", 'a')
-      log << "\n\nStarting inventory sync process: #{Time.now}\n\n"
+      log << "Starting inventory sync process: #{Time.now}\n\n"
 
       skus = newgistics_stock_items.map { |si| si["sku"] }
-      variants = Spree::Variant.where(sku: skus).includes(:stock_items)
+      variants = Spree::Variant.where(sku: skus, is_master: false).includes(:stock_items)
 
       newgistics_stock_items.each do |newgistic_stock_item|
-        variant = variants.find { |v| v.sku == newgistic_stock_item["sku"] }#Spree::Variant.where(is_master: false).find_by(sku: newgistic_stock_item["sku"])
+        variant = variants.find { |v| v.sku == newgistic_stock_item["sku"] }
         next unless variant
         ## Since newgistics is the only stock location, set 1 as stock_location id.
         ## TODO: add support for multiple stock locations.
@@ -43,7 +43,7 @@ module Workers
             end
           end
 
-          log << "Not synced #{variant.sku}\n"
+          log << "Not synced #{newgistic_stock_item['sku']}\n"
           log << "On hold - spree: #{stock_item.count_on_hold} NG: #{ng_pending_quantity}\n"
           log << "Avaliable - spree: #{stock_item.count_on_hand} NG: #{ng_available_quantity}\n"
           log << "Variant is used #{unsynced_on_hold} times in unsynced orders\n"
