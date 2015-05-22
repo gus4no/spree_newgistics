e9c6dc36f2394df320fc0173c7e745585bf3f3ea
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-05-22 21:31:57 +0300

    tests on sync reconciliation

--- spec/workers/inventory_puller_spec.rb
+++ spec/workers/inventory_puller_spec.rb
@@ -31,46 +31,99 @@ describe Workers::InventoryPuller do
       let(:variant) { create :variant, sku: '1234' }
       let(:random) { Random.new }
 
-      it "should change count_on_hold on data from NG" do
-        first_stock_item = variant.stock_items.first
-        count_on_hold = first_stock_item.count_on_hold + random.rand(100) + 1
-        response = [
-          {
-            "id"=>"1148187",
-            "sku"=>"1234",
-            "currentQuantity"=>"6",
-            "receivingQuantity"=>"0",
-            "arrivedPutAwayQuantity"=>"0",
-            "kittingQuantity"=>"0",
-            "returnsQuantity"=>"0",
-            "pendingQuantity"=>"#{count_on_hold}",
-            "availableQuantity"=>"#{first_stock_item.count_on_hand}",
-            "backorderedQuantity"=>"0"
-          }
-        ]
+      context "there is no not sync'ed orders" do
+        it "should change count_on_hold on data from NG" do
+          first_stock_item = variant.stock_items.first
+          count_on_hold = first_stock_item.count_on_hold + random.rand(100) + 1
+          response = [
+            {
+              "id"=>"1148187",
+              "sku"=>"1234",
+              "currentQuantity"=>"6",
+              "receivingQuantity"=>"0",
+              "arrivedPutAwayQuantity"=>"0",
+              "kittingQuantity"=>"0",
+              "returnsQuantity"=>"0",
+              "pendingQuantity"=>"#{count_on_hold}",
+              "availableQuantity"=>"#{first_stock_item.count_on_hand}",
+              "backorderedQuantity"=>"0"
+            }
+          ]
 
-        expect{subject.update_inventory(response)}.to change{variant.stock_items.first.count_on_hold}.to(count_on_hold)
+          expect{subject.update_inventory(response)}.to change{variant.stock_items.first.count_on_hold}.to(count_on_hold)
+        end
+
+        it "should change count_on_hold on data from NG" do
+          first_stock_item = variant.stock_items.first
+          count_on_hand = first_stock_item.count_on_hand + random.rand(100) + 1
+          response = [
+            {
+              "id"=>"1148187",
+              "sku"=>"1234",
+              "currentQuantity"=>"6",
+              "receivingQuantity"=>"0",
+              "arrivedPutAwayQuantity"=>"0",
+              "kittingQuantity"=>"0",
+              "returnsQuantity"=>"0",
+              "pendingQuantity"=>"#{first_stock_item.count_on_hold}",
+              "availableQuantity"=>"#{count_on_hand}",
+              "backorderedQuantity"=>"0"
+            }
+          ]
+
+          expect{subject.update_inventory(response)}.to change{variant.stock_items.first.count_on_hand}.to(count_on_hand)
+        end
       end
 
-      it "should change count_on_hold on data from NG" do
-        first_stock_item = variant.stock_items.first
-        count_on_hand = first_stock_item.count_on_hand + random.rand(100) + 1
-        response = [
-          {
-            "id"=>"1148187",
-            "sku"=>"1234",
-            "currentQuantity"=>"6",
-            "receivingQuantity"=>"0",
-            "arrivedPutAwayQuantity"=>"0",
-            "kittingQuantity"=>"0",
-            "returnsQuantity"=>"0",
-            "pendingQuantity"=>"#{first_stock_item.count_on_hold}",
-            "availableQuantity"=>"#{count_on_hand}",
-            "backorderedQuantity"=>"0"
-          }
-        ]
+      context "there are not sync'ed orders" do
+        # BEWARE: fragile tests here
 
-        expect{subject.update_inventory(response)}.to change{variant.stock_items.first.count_on_hand}.to(count_on_hand)
+        let(:order) { create :order_with_line_items, state: 'complete', posted_to_newgistics: false }
+        let(:variant) { create :variant, sku: '1234' }
+
+        before :each do
+          # prepare order
+          first_line_item = order.line_items.first
+          first_line_item.variant_id = variant.id
+          first_line_item.save
+
+          first_line_item_variant = first_line_item.variant_id
+          first_line_item_qty = first_line_item.quantity
+
+          # prepare variant
+          first_stock_item = variant.stock_items.first
+          first_stock_item.stock_location_id = 1 # replace when we will have different stock_location_id's
+          first_stock_item.save
+
+          count_on_hand = 100
+          count_on_hold = 0
+
+          @response = [
+            {
+              "id"=>"#{first_line_item_variant}",
+              "sku"=>"1234",
+              "currentQuantity"=>"6",
+              "receivingQuantity"=>"0",
+              "arrivedPutAwayQuantity"=>"0",
+              "kittingQuantity"=>"0",
+              "returnsQuantity"=>"0",
+              "pendingQuantity"=>"#{count_on_hold}",
+              "availableQuantity"=>"#{count_on_hand}",
+              "backorderedQuantity"=>"0"
+            }
+          ]
+
+          @new_count_on_hand = count_on_hand - first_line_item_qty
+          @new_count_on_hold = count_on_hold + first_line_item_qty
+        end
+
+        it "should do reconciliation on count_on_hand" do
+          expect{subject.update_inventory(@response)}.to change{variant.stock_items.first.count_on_hand}.to(@new_count_on_hand)
+        end
+
+        it "should do reconciliation on count_on_hold" do
+          expect{subject.update_inventory(@response)}.to change{variant.stock_items.first.count_on_hold}.to(@new_count_on_hold)
+        end
       end
     end
   end
