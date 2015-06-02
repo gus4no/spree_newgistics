d13dde4de01ee942d2e6773d143760b078e134c3
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-06-02 18:22:15 +0400

    failing test

--- spec/workers/orders_puller_spec.rb
+++ spec/workers/orders_puller_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 describe Workers::OrdersPuller do
   describe "#update_shipments" do
 
+    let(:order) { create :order_with_line_items, state: 'complete' }
+
     it "must enqueue product review email when status is SHIPPED" do
+      expect(order).to receive(:send_product_review_email)
     end
 
   end
