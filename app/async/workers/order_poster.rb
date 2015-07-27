2d4bb400ce594699ce1591d9d63409962ace451e
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-07-27 20:54:18 +0400

    improved error handling

--- app/async/workers/order_poster.rb
+++ app/async/workers/order_poster.rb
@@ -14,22 +14,17 @@ module Workers
         log << "Order is being sent to Newgistics\n"
         document = Spree::Newgistics::DocumentBuilder.build_shipment(order.shipments)
         response = Spree::Newgistics::HTTPManager.post('/post_shipments.aspx', document)
-        if response.status <= 299
+        if update_success?(response, order.number)
           errors = Nokogiri::XML(response.body).css('errors').children
-          if !errors.any?
-            log << "NG responded with status #{response.status}, processing order\n"
-            order.update_attributes({posted_to_newgistics: true, newgistics_status: 'RECEIVED'})
-          else
-            log << "NG responded with status #{response.status} with errors:\n"
-            errors.each { |e| log << "#{e}\n" }
-          end
+          log << "NG responded with status #{response.status}, processing order\n"
+          order.update_attributes({posted_to_newgistics: true, newgistics_status: 'RECEIVED'})
           log.close
-        elsif response.status > 399
-          log << "NG responded with status #{response.status}\n"
-          log.close
-          raise "Newgistics response failed, status: #{response.status}"
         else
+          errors = Nokogiri::XML(response.body).css('errors').children
+          log << "NG responded with status #{response.status} and response has errors\n"
+          errors.each { |e| log << "#{e} \n" }
           log.close
+          raise "Newgistics response failed, status: #{response.status} and response has errors"
         end
       else
         log << "Order is alredy sent to Newgistics\n"
