27e1487f69b516d2f5836a1b85dfa5e34b53689e
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-08-12 16:44:49 +0400

    test newgistics_status change

---
+++ spec/workers/orders_puller_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Workers::OrdersPuller do
+
+  describe "#update_shipments" do
+
+    context "when newgistics status differs" do
+      let(:order) { create :order, newgistics_status: 'RECEIVED', state: 'complete' }
+      let(:response) do [{
+        'OrderID' => order.number,
+        'FirstName' => 'John',
+        'LastName' => 'Smith',
+        'Address1' => 'Somewhere in US',
+        'City' => 'Anycity',
+        'State' => 'CA',
+        'PostalCode' => '12345',
+        'Country' => 'UNITED STATES',
+        'Phone' => '9871231233'
+      }] end
+
+      # missing CANCELED status
+      %w{BACKORDER BADADDRESS BADSKUHOLD CNFHOLD INVHOLD ONHOLD
+        PICKVERIFIED PRINTED RECEIVED RETURNED SHIPPED UPDATED VERIFIED}.each do |newgistics_status|
+
+        it "should set newgistics status to #{newgistics_status}" do
+          Spree::Order.any_instance.stub(:send_product_review_email)
+          response.each { |s| s['ShipmentStatus'] = newgistics_status }
+
+          expect { subject.update_shipments(response) }.not_to raise_error
+          order.reload
+          expect(order.newgistics_status).to eq(newgistics_status)
+        end
+
+      end
+
+    end
+
+  end
+
+end
