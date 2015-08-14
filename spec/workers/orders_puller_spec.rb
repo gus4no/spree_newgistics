27c226d3d521c9dd7c316cb057df6b1c9bf02ffb
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-08-14 17:54:44 +0400

    test on several order updates

--- spec/workers/orders_puller_spec.rb
+++ spec/workers/orders_puller_spec.rb
@@ -36,24 +36,92 @@ describe Workers::OrdersPuller do
     end
 
     context "when exception occurs" do
-      let(:order) { create :order, newgistics_status: 'SHIPPED', state: 'delivery' }
-      let(:response) do [{
-        'OrderID' => order.number,
-        'FirstName' => 'John',
-        'LastName' => 'Smith',
-        'Address1' => 'Somewhere in US',
-        'City' => 'Anycity',
-        'State' => 'CA',
-        'PostalCode' => '12345',
-        'Country' => 'UNITED STATES',
-        'Phone' => '9871231233',
-        'ShipmentStatus' => 'CANCELED'
-      }] end
+      context "when there is only one order in response" do
+        let(:order) { create :order, newgistics_status: 'SHIPPED', state: 'delivery' }
+        let(:response) do [{
+          'OrderID' => order.number,
+          'FirstName' => 'John',
+          'LastName' => 'Smith',
+          'Address1' => 'Somewhere in US',
+          'City' => 'Anycity',
+          'State' => 'CA',
+          'PostalCode' => '12345',
+          'Country' => 'UNITED STATES',
+          'Phone' => '9871231233',
+          'ShipmentStatus' => 'CANCELED'
+        }] end
 
-      it "should not throw an exception" do
-        Spree::Order.any_instance.stub(:send_product_review_email)
+        it "should not throw an exception" do
+          Spree::Order.any_instance.stub(:send_product_review_email)
 
-        expect { subject.update_shipments(response) }.not_to raise_error
+          expect { subject.update_shipments(response) }.not_to raise_error
+        end
+      end
+
+      context "when there are several orders in response" do
+        let(:order_one) { create :order, newgistics_status: 'UPDATED', state: 'complete' }
+        let(:broken_order) { create :order, newgistics_status: 'UPDATED', state: 'delivery' }
+        let(:order_two) { create :order, newgistics_status: 'UPDATED', state: 'complete' }
+        let(:response) do [{
+          'OrderID' => order_one.number,
+          'FirstName' => 'John',
+          'LastName' => 'Smith',
+          'Address1' => 'Somewhere in US',
+          'City' => 'Anycity',
+          'State' => 'CA',
+          'PostalCode' => '12345',
+          'Country' => 'UNITED STATES',
+          'Phone' => '9871231233',
+          'ShipmentStatus' => 'SHIPPED'
+        }, {
+          'OrderID' => broken_order.number,
+          'FirstName' => 'John',
+          'LastName' => 'Smith',
+          'Address1' => 'Somewhere in US',
+          'City' => 'Anycity',
+          'State' => 'CA',
+          'PostalCode' => '12345',
+          'Country' => 'UNITED STATES',
+          'Phone' => '9871231233',
+          'ShipmentStatus' => 'CANCELED'
+        }, {
+          'OrderID' => order_two.number,
+          'FirstName' => 'John',
+          'LastName' => 'Smith',
+          'Address1' => 'Somewhere in US',
+          'City' => 'Anycity',
+          'State' => 'CA',
+          'PostalCode' => '12345',
+          'Country' => 'UNITED STATES',
+          'Phone' => '9871231233',
+          'ShipmentStatus' => 'SHIPPED'
+        }] end
+
+        it "should update first order" do
+          Spree::Order.any_instance.stub(:send_product_review_email)
+
+          expect { subject.update_shipments(response) }.not_to raise_error
+
+          order_one.reload
+
+          expect(order_one.newgistics_status).to eq('SHIPPED')
+          order_one.shipments.each do |s|
+            expect(s.status).to be(:shipped)
+          end
+        end
+
+        it "should update second order" do
+          Spree::Order.any_instance.stub(:send_product_review_email)
+
+          expect { subject.update_shipments(response) }.not_to raise_error
+
+          order_two.reload
+
+          expect(order_two.newgistics_status).to eq('SHIPPED')
+          order_two.shipments.each do |s|
+            expect(s.status).to be(:shipped)
+          end
+        end
       end
 
     end
